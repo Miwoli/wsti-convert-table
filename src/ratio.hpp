0998cb5a80ccba59b1e8fcd3a58d879f1a76d950
Michał Woliński <miwoli98@gmail.com>
2019-10-26 15:21:47 +0200

    Initial commit

---
+++ src/ratio.hpp
@@ -0,0 +1,23 @@
+#ifndef RATIO_HPP
+#define RATIO_HPP
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+class Ratio {
+    double meter_min;
+    double meter_max;
+    double yard = 1.0936133;
+    double foot = 3.2808399;
+    double inch = 39.3700787;
+    double mile = 0.000621371192;
+    double step;
+    std::vector<std::vector<std::string>> results;
+
+    public:
+
+        
+};
+
+#endif
